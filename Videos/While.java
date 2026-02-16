0f4d39930ae0261d85c56b7e5a428c39cf11eae0
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-16 16:46:58 +0530

    add while loop file

---
+++ Videos/While.java
@@ -0,0 +1,14 @@
+class While{
+    public static void main(String[] args) {
+        int i = 1;
+        // while (i<=3) { 
+        //     System.out.println(i);
+        //     i++;
+        // }
+
+        int j = 100;
+        while (j<=200) { 
+            System.out.println(j++);
+        }
+    }
+}

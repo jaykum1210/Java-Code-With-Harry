39b102e63f0f14cec47accec648d2fe1a297fc56
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-17 22:50:29 +0530

    add this and super file

---
+++ Videos/ThisSuper.java
@@ -0,0 +1,27 @@
+class A{
+    private int a;
+    
+    public A(int a){
+        this.a = a;
+    }
+
+    public int getA(){
+        return a;
+    }
+}
+
+class B extends A{
+    public B(int a){
+        super(a);
+        System.out.println("Hello");
+    }
+}
+
+public class ThisSuper{
+    public static void main(String[] args) {
+        A a = new A(10);
+        System.out.println("Value : " + a.getA());
+        B b = new B(20);
+        System.out.println("Value : " + b.getA());
+    }
+}

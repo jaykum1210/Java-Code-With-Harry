e2787b47e5ced6327abad6f56ca594c1c98060d3
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-18 22:56:04 +0530

    upload all files to eclipse

---
+++ Videos/Packages.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+// import java.util.Scanner;
+
+public class Packages{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        // java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("Value : " + a);
+    }
+}
---
+++ Videos/examplepackage/Jay1.java
@@ -0,0 +1,10 @@
+package examplepackage;
+
+public class Jay1 {
+   public Jay1() {
+   }
+
+   public static void main(String[] var0) {
+      System.out.println("Hello hii");
+   }
+}

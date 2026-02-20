21b0d11078b16056ec0870cc082659d2967f0aab
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-20 18:53:06 +0530

    add 3 files

---
+++ Patterns/Pattern3.java
@@ -0,0 +1,17 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern3 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int a = sc.nextInt();
+        for(int i = 0;i<a;i++){
+            for(int j = 0;j<a-i;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

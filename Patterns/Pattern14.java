30c6d358325193f29f00a426ea6ac642575fd22a
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-20 20:19:50 +0530

    add pattern 12 file

--- Patterns/Pattern14.java
+++ Patterns/Pattern14.java
@@ -2,7 +2,7 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class Pattern6 {
+public class Pattern14 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number : ");
@@ -11,9 +11,10 @@ public class Pattern6 {
             for (int j = 0; j < i; j++) {
                 System.out.print("  ");
             }
+            int num = 0;
             for (int j = 0; j <=(2 * (a - i) - 1); j++) {
                 if (j % 2 != 0) {
-                    System.out.print("* ");
+                    System.out.print(++num + " ");
                 } else {
                     System.out.print("  ");
                 }

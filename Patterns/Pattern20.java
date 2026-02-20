d79e5b2bbdfdced07ec8db119350f0b00384bd3a
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-20 21:02:45 +0530

    add Pattern20 file

---
+++ Patterns/Pattern20.java
@@ -0,0 +1,26 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern20 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int a = sc.nextInt();
+        for(int i = 0;i<a;i++){
+            for(int j = 0;j<a-i-1;j++){
+                System.out.print("  ");
+            }
+            int num = i+1;
+            for(int j = 0;j<=(2*i+1);j++){
+                if(j%2!=0){
+                    System.out.print(num + " ");
+                }
+                else{
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

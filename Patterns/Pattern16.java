e9918b2d4db4302d93e8cd77a1a39ab3ab112f02
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-20 20:35:01 +0530

    add Pattern 16 file

--- Patterns/Pattern16.java
+++ Patterns/Pattern16.java
@@ -1,5 +1,25 @@
 package Patterns;
 
+import java.util.Scanner;
+
 public class Pattern16 {
-    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int a = sc.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            int num = i;
+            for (int j = 0; j <=(2 * (a - i) - 1); j++) {
+                if (j % 2 != 0) {
+                    System.out.print(++num + " ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
 }

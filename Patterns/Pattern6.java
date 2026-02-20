b03b32bdd7a327b9755c67601747a245b43530b0
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-20 19:09:14 +0530

    add pattern 6 file

---
+++ Patterns/Pattern6.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern6 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int a = sc.nextInt();
+        for (int i = 0; i < a; i++) {
+            for (int j = 0; j < i; j++) {
+                System.out.print("  ");
+            }
+            for (int j = 0; j < (2 * (a - i) - 1); j++) {
+                if (j % 2 != 0) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

49e199deafb4757adb553d333d925702e28c4781
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-20 20:07:01 +0530

    add pattern 11 file

---
+++ Patterns/Pattern11.java
@@ -0,0 +1,24 @@
+package Patterns;
+
+import java.util.Scanner;
+
+public class Pattern11 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Number : ");
+        int a = sc.nextInt();
+        for(int i = 0;i<2*a;i++){
+            if(i<a){
+                for(int j = 0;j<a-i;j++){
+                    System.out.print("* ");
+                }
+            }
+            else{
+                for(int j = 0;j<(i-a+1);j++){
+                    System.out.print("* ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}

53763fabed812bf57ad36fc7ca7fc22cda08d273
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-22 22:32:17 +0530

    add Pattern32 file

--- Patterns/Pattern32.java
+++ Patterns/Pattern32.java
@@ -2,12 +2,14 @@ package Patterns;
 
 import java.util.Scanner;
 
-public class Pattern7 {
+public class Pattern32 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.print("Enter Number : ");
         int a = sc.nextInt();
+        int num;
         for (int i = 0; i < 2 * a-1; i++) {
+            num = 0;
             if (i < a) {
                 for (int j = 0; j < a - i - 1; j++) {
                     System.out.print("  ");
@@ -16,18 +18,32 @@ public class Pattern7 {
                     if (j % 2 != 0) {
                         System.out.print("  ");
                     } else {
-                        System.out.print("* ");
+                        num++;
+                        if(j == 0 || j == (2*i)){
+                        System.out.print(num + " ");
+                        }
+                        else{
+                        System.out.print("  ");
+                        }
                     }
                 }
                 System.out.println();
             } else {
-                for (int j = 0; j < i - a; j++) {
+                for (int j = 0; j <=i - a; j++) {
                     System.out.print("  ");
                 }
-                for (int j = 0; j < (2 * (2 * a - i) - 1); j++) {
-                    if (j % 2 != 0) {
-                        System.out.print("* ");
-                    } else {
+                for(int j = 0; j < (2 * (2 * a - i) - 3);j++){
+                    if(j%2==0){
+                        num++;
+                        if(j == 0 || j == (2*(2*a-i)-4)){
+                            // System.out.print("* ");
+                            System.out.print(num + " ");
+                        }
+                        else{
+                            System.out.print("  ");
+                        }
+                    }
+                    else{
                         System.out.print("  ");
                     }
                 }

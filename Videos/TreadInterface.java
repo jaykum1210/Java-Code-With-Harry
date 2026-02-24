a784f9ac33949ccc433b27e0a2b0e60f677adcba
jaykum1210 <jaykumawat1210@gmail.com>
2026-02-24 18:23:13 +0530

    add file

---
+++ Videos/TreadInterface.java
@@ -0,0 +1,33 @@
+class MyThread11 implements Runnable{
+	public void run() {
+		int i = 0;
+		while(i<10) {
+			System.out.println(i++);
+		}
+	}
+}
+
+class MyThread21 implements Runnable{
+	char ch = 'A';
+	public void run() {
+		while((ch-'A')<10) {
+			System.out.println(ch++);
+		}
+	}
+}
+
+public class TreadInterface {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		MyThread11 r1 = new MyThread11();
+		Thread t1 = new Thread(r1);
+		
+		MyThread21 r2 = new MyThread21();
+		Thread t2 = new Thread(r2);
+		
+		t1.start();
+		t2.start();
+	}
+
+}
